b25f40e43d060693b018c918119ea470d1330307
RajanSharma04 <Sharmarajan4565@gmail.com>
2024-01-15 13:59:51 +0530

    addtions

---
+++ Assets/js/script.js
@@ -0,0 +1,10 @@
+// slideshow slider
+  $('.slideshow-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 1500
+  });
