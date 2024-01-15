3c9b49d54d038c0d0bc5f32d27bede65856a62c4
RajanSharma04 <Sharmarajan4565@gmail.com>
2024-01-15 16:40:48 +0530

    addtions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,3 +8,23 @@
     autoplay: true,
     autoplaySpeed: 1500
   });
+
+
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  tablinks = document.getElementsByClassName("tablinks");
+
+  tabcontent[0].style.display = "block";
+  tablinks[0].className += " active";
+
+//   tabs
+function openCity(evt, cityName) {
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
